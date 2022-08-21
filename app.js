bbb8e12a0ec53c2b002b702fe1a8679b0f5cf3d8
Taieb OUBERKA <obk@Taiebs-MBP.fios-router.home>
2022-08-21 11:32:13 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+
+class App extends React.Component {
+  constructor(props) { //every time you add a component, you NEED to bind it
+      super()
+      this.handleChange = this.handleChange.bind(this)
+      this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  state = {
+      products: products,
+      item: '',
+      units: '',
+      quantity: 0
+      // value: ''
+  }
+  handleChange = (event) => {
+      // this.state.value = e.target.value // this is wrong, we cannot mutate state directly
+      this.setState({ [event.target.id]: event.target.value })
+  }
+  handleSubmit = (event) => {
+      event.preventDefault()
+      const newItem = {
+          item: this.state.item,
+          units: this.state.units,
+          quantity: this.state.quantity
+      }
+      this.setState({
+          products: [newItem, ...this.state.products],
+          item: '',
+          units: '',
+          quantity: 0
+      })
+  }
+  toggleHiring = () => {
+      this.setState({isPurchased: !isPurchased})
+    }
+  render() {
+      return (
+          <div className="App">
+              <h1 onClick={this.toggleHiring}> Welcome to Groceries store! </h1>
+              {!this.state.isPurchased ? <h2>Yes, we are seling </h2>: <h2>Sorry, try again tomorrow</h2>}
+              <form onSubmit={this.handleSubmit}>
+                  <label htmlFor='item'>Item name</label>
+                  <input type='text'  className="input" value={this.state.item} onChange={this.handleChange} id='item' placeholder='item of product' />
+                  <br />
+                  <label htmlFor='units'>Units</label>
+                  <input type='number'  className="input" value={this.state.units} onChange={this.handleChange} id='units' />
+                  <br />
+                  <label htmlFor='quantity'>Quantity</label>
+                  <input type='textarea'  className="input" value={this.state.quantity} onChange={this.handleChange} id='quantity' />
+                  <input type="submit" className="btn" /><br/>
+              </form>
+              <div>
+                  <h2>Preview our new item</h2>
+                  <h3>{this.state.item}</h3>
+                  <h4>{this.state.units}</h4>
+                  <h5>{this.state.quantity}</h5>
+              </div>
+              <ul>
+                  {this.state.products.map(product => {
+                      return (
+                          <li>{product.item}  {product.units}</li>)
+                  }
+                  )
+                  }
+              </ul>
+          </div>
+      )
+  }
+}
+ReactDOM.render(
+  <App />,
+  document.querySelector('.container')
+)
---
+++ data.js
@@ -0,0 +1,23 @@
+const products = [
+    {
+        item: 'shoes',
+        units: '3',
+        quantity: 20
+        
+      },
+      {
+        item: 'shoes',
+        units: '4',
+        quantity: 2
+        
+      },
+      {
+        item: 'shoes-p',
+        units: '9',
+        quantity: 77,
+        
+      }
+
+]
+
+
